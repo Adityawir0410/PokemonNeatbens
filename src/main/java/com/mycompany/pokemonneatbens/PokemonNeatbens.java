4d1a0d978bde53c3280d2c8f6be1d37c93b22d58
Aditya Wirayudha <83379121+Adityawir0410@users.noreply.github.com>
2024-05-14 07:35:02 +0700

    Initial commit

---
+++ src/main/java/com/mycompany/pokemonneatbens/PokemonNeatbens.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pokemonneatbens;
+
+/**
+ *
+ * @author aditya wirz
+ */
+public class PokemonNeatbens {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
